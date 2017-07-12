f0c8bb979f43af1465bcaf645feef85a4052ff52
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-07-12 19:27:58 +0100

    [speed] Add --timing option to speed benchmarks sp dp

--- python/speed_benchmark_sp.py
+++ python/speed_benchmark_sp.py
@@ -6,19 +6,23 @@ from __future__ import (division, print_function, absolute_import,
 import numpy as np
 import os
 import datetime
+import textwrap
+import timeit
+import optparse
 
 from utils import generate_random_vis, create_reference_image, create_sampling_points
 import galario
 
-import optparse
 p = optparse.OptionParser()
 p.add_option("--gpu", action="store_true", dest="USE_GPU", default=False,
              help="Use GPU version of galario")
+p.add_option("--timing", action="store_true", dest="TIMING", default=False,
+             help="Time chi2()")
 
 (options, args) = p.parse_args()
 
 if options.USE_GPU:
-    if galario.HAVE_CUDA and options.USE_GPU:
+    if galario.HAVE_CUDA:
 
         from galario import single_cuda as acc_lib
 
@@ -66,10 +70,50 @@ if __name__ == '__main__':
     size = 4096
     nsamples = int(1e6)
     real_type = "float32"
-    input_sample = setup_sample(size, nsamples, real_type)
-    input_chi2 = setup_chi2(size, nsamples, real_type)
 
-    acc_lib.sample(*input_sample)
+    if not options.TIMING:
+        input_sample = setup_sample(size, nsamples, real_type)
+        input_chi2 = setup_chi2(size, nsamples, real_type)
 
-    acc_lib.chi2(*input_chi2)
+        acc_lib.sample(*input_sample)
+
+        acc_lib.chi2(*input_chi2)
+
+    else:
+        omp_num_threads = os.environ.get('OMP_NUM_THREADS', 1)
+
+        cycles = 5
+        number = 1
+        t = timeit.Timer('acc_lib.chi2(*x)'.format(acc_lib),
+                         setup=textwrap.dedent("""
+                            from __main__ import setup_chi2, acc_lib
+                            x = setup_chi2(int({0}), int({1}), "{2}")
+                            # from galario import double_cuda as acc_lib
+                            # ngpus = acc_lib.ngpus()
+                            # acc_lib.use_gpu(0) #max(0, ngpus - 1))
+                            # acc_lib.threads_per_block()
+                            """.format(size, nsamples, real_type)))
+
+        str_headers = "\t".join(["size", "nsamples", "real", "OMP", "Ttot", "Tavg", "Tstd", "Tmin"])
+
+        filename = "timings_"
+        if options.USE_GPU:
+            filename += "GPU"
+        else:
+            filename += "CPU_OMP_NUM_THREADS_{}".format(omp_num_threads)
+        filename += "_{}.txt".format(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+
+        t_results = t.repeat(cycles, number)
+        str_results = "{}\t{:e}\t{}\t{}\t{:e}\t{:e}\t{:e}\t{:e}".format(size, nsamples, real_type, omp_num_threads, np.sum(t_results),
+                                  np.average(t_results), np.std(t_results), np.min(t_results))
+
+        with open(filename, 'w') as f:
+            f.write(str_headers + "\n")
+            f.write(str_results + "\n")
+            f.write(" |--> timings: {}".format(t_results) + "\n")
+
+        print(str_headers)
+        print(str_results)
+        print(t_results)
+        print("Log saved in {}".format(filename))
 
