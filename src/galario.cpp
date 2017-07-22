ef09392b863d132b7a8c98b0b35c3d6efa03497c
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-07-22 21:42:11 +0100

    [cpp] Fix uv_idx_r2c: fabs instead of abs needed. Now all CPU and GPU tests pass.

--- src/galario.cpp
+++ src/galario.cpp
@@ -788,8 +788,8 @@ void _galario_get_uv_idx(int nx, dreal du, int nd, void* u, void* v, void* indu,
 #ifdef __CUDACC__
 __host__ __device__
 #endif
-inline void uv_idx_R2C_core(int const i, int const half_nx, dreal const du, dreal const* const u, dreal const* const v, dreal* const __restrict__ indu, dreal*  const __restrict__ indv) {
-    indu[i] = abs(u[i])/du;
+        (int const i, int const half_nx, dreal const du, dreal const* const u, dreal const* const v, dreal* const __restrict__ indu, dreal*  const __restrict__ indv) {
+    indu[i] = fabs(u[i])/du;
     indv[i] = half_nx + v[i]/du;
 
     if (u[i] < 0.) indv[i] *= -1.;
@@ -1220,7 +1220,7 @@ void galario_chi2(int nx, dreal* realdata, dreal dRA, dreal dDec, dreal du, int 
      // ################################
      // ########### CLEANUP ############
      // ################################
-     float elapsed=0;
+     /*float elapsed=0;
      cudaEvent_t start, stop;
      CCheck(cudaEventCreate(&start));
      CCheck(cudaEventCreate(&stop));
@@ -1242,7 +1242,7 @@ void galario_chi2(int nx, dreal* realdata, dreal dRA, dreal dDec, dreal du, int 
      CCheck(cudaEventDestroy(start));
      CCheck(cudaEventDestroy(stop));
      printf("The total time to free memory in chi2 is %.3f ms", elapsed);
-
+     */
 #else
 
      dcomplex* fint = (dcomplex*) malloc(sizeof(dcomplex)*nd);
