e1799a55c307cbde79b8108ff88a5bdfc670d389
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2017-07-27 10:46:05 +0200

    [src] Apply `const` in func. declaration
    Some open issues remain, see #57

--- src/galario.h
+++ src/galario.h
@@ -7,19 +7,19 @@ extern "C"
 {
 #endif /* __cplusplus */
     /* Main user functions */
-    void galario_sample(int nx, int ny, dreal* data, dreal dRA, dreal dDec, dreal du, int nd, dreal* u, dreal* v, dcomplex* fint);
-    void galario_chi2(int nx, int ny, dreal* data, dreal dRA, dreal dDec, dreal du, int nd, dreal* u, dreal* v, dreal* fobs_re, dreal* fobs_im, dreal* weights, dreal* chi2);
+    void galario_sample(int nx, int ny, dreal* data, dreal dRA, dreal dDec, dreal du, int nd, const dreal* u, const dreal* v, dcomplex* fint);
+    void galario_chi2(int nx, int ny, dreal* data, dreal dRA, dreal dDec, dreal du, int nd, const dreal* u, const dreal* v, const dreal* fobs_re, const dreal* fobs_im, const dreal* weights, dreal* chi2);
 
     /* Interface for the experts */
-    dcomplex* galario_fft2d(int nx, int ny, dreal* data);
+    dcomplex* galario_fft2d(int nx, int ny, const dreal* data);
     void galario_fftshift(int nx, int ny, dreal* data);
     void galario_fftshift_axis0(int nx, int ny, dcomplex* data);
-    void galario_interpolate(int nx, int ny, dcomplex *data, int nd, dreal *u, dreal *v, dcomplex *fint);
+    void galario_interpolate(int nx, int ny, const dcomplex* data, int nd, const dreal* u, const dreal* v, dcomplex* fint);
     void galario_apply_phase_2d(int nx, int ny, dcomplex* data, dreal dRA, dreal dDec);
-    void galario_apply_phase_sampled(dreal dRA, dreal dDec, int nd, dreal* u, dreal* v, dcomplex* fint);
-    void galario_get_uv_idx(int nx, int ny, dreal du, int nd, dreal* u, dreal* v, dreal* indu, dreal* indv);
-    void galario_get_uv_idx_R2C(int nx, int ny, dreal du, int nd, dreal* u, dreal* v, dreal* indu, dreal* indv);
-    void galario_reduce_chi2(int nd, dreal* fobs_re, dreal* fobs_im, dcomplex* fint, dreal* weights, dreal* chi2);
+    void galario_apply_phase_sampled(dreal dRA, dreal dDec, int nd, const dreal* u, const dreal* v, dcomplex* fint);
+    void galario_get_uv_idx(int nx, int ny, dreal du, int nd, const dreal* u, const dreal* v, const dreal* indu, dreal* indv);
+    void galario_get_uv_idx_R2C(int nx, int ny, dreal du, int nd, const dreal* u, const dreal* v, dreal* indu, dreal* indv);
+    void galario_reduce_chi2(int nd, const dreal* fobs_re, const dreal* fobs_im, const dcomplex* fint, const dreal* weights, dreal* chi2);
 
     /* Required for multithreading */
     void galario_init();
