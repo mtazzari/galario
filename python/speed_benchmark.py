4c4f94201de276f302b7b9c028a0d98f7391d82c
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2017-08-25 13:40:39 +0000

    [speed] Rewrite benchmark to do loop over threads
    This save the enormous overhead of calling setup each time. Now only
    once for one resolution

--- python/speed_benchmark.py
+++ python/speed_benchmark.py
@@ -16,45 +16,48 @@ import galario
 from galario import au, pc, cgs_to_Jy
 from utils import *
 
-import optparse
-p = optparse.OptionParser()
-p.add_option("--gpu", action="store_true", dest="USE_GPU", default=False,
+import argparse
+p = argparse.ArgumentParser()
+p.add_argument("--gpu", action="store_true", dest="USE_GPU", default=False,
              help="Use GPU version of galario")
-p.add_option("--gpu_id", action="store", dest="gpu_id", default=0, type=int,
+p.add_argument("--cpu", action="store_true", dest="USE_CPU", default=True,
+             help="Use CPU version of galario")
+p.add_argument("--gpu_id", action="store", dest="gpu_id", default=0, type=int,
              help="Choose index of GPU if several are available.  Check `watch -n 0.1 nvidia-smi` to see which gpu is used during test execution")
-p.add_option("--timing", action="store_true", dest="TIMING", default=False,
+p.add_argument("--timing", action="store_true", dest="TIMING", default=False,
              help="Time chi2()")
-p.add_option("--cycles", action="store", dest="cycles", default=5, type=int,
+p.add_argument("--cycles", action="store", dest="cycles", default=5, type=int,
              help="Number of cycles in calls to test")
-p.add_option("--size", action="store", dest="size", default=4096, type=int,
+p.add_argument("--size", action="store", dest="size", default=4096, type=int,
              help="Square input image size")
-p.add_option("--tpb", action="store", dest="tpb", default=0, type=int,
-             help="Threads per block on the GPU")
-p.add_option("--dtype", action="store", dest="dtype", default='float64',
+p.add_argument("--tpb", type=int, nargs='+', help="Threads per block on the GPU", default=[16])
+p.add_argument("--ompnthreads", type=int, nargs='+', help="$OMP_NUM_THREADS", default=[1])
+p.add_argument("--dtype", action="store", dest="dtype", default='float64',
              help="Data type of the input image")
-p.add_option("--output", action="store", dest="output", default="",
+p.add_argument("--output", action="store", dest="output", default="",
              help="Name of output file")
-p.add_option("--output_header", action="store_true", dest="output_header",
+p.add_argument("--output_header", action="store_true", dest="output_header",
              help="Only create output file and print header, then quit.")
-p.add_option("--image", action="store_true", default=False,
+p.add_argument("--image", action="store_true", default=False,
              help="If True computes from Image, else computes from profile.")
+p.add_argument("--no-verbose", action="store_true", default=False,
+               help="Print timing to stdout")
 
-(options, args) = p.parse_args()
+options = p.parse_args()
+# print(options)
 
 if options.USE_GPU:
     if galario.HAVE_CUDA:
 
-        from galario import double_cuda as acc_lib
+        from galario import double_cuda as acc_lib_cuda
 
-        acc_lib.use_gpu(options.gpu_id)
+        acc_lib_cuda.use_gpu(options.gpu_id)
 
-        if options.tpb:
-            acc_lib.threads_per_block(options.tpb)
     else:
         print("Option --gpu not valid. galario.HAVE_CUDA is {}. Terminating.".format(galario.HAVE_CUDA))
+        exit(1)
 
-else:
-    from galario import double as acc_lib
+from galario import double as acc_lib_cpu
 
 
 def setup_sampleImage(nxy, nsamples):
@@ -99,7 +102,7 @@ def setup_chi2(size, nsamples):
 
 def setup_chi2Profile(nxy, nsamples):
 
-    sys.stdout.write("Setup chi2Image...")
+    sys.stdout.write("Setup chi2Profile...")
     sys.stdout.flush()
 
     pars = {'wle_m': 0.00088, 'dRA': 2.3, 'dDec': 3.2, 'PA': 88., 'nxy': 4096}
@@ -134,58 +137,67 @@ def setup_chi2Profile(nxy, nsamples):
 
     return ints, Rmin, dR, nxy, dxy, dist, udat/wle_m, vdat/wle_m, x.real.copy(), x.imag.copy(), w, inc/180.*np.pi, dRA, dDec
 
-if __name__ == '__main__':
+def do_timing(options, input_data, gpu=False, tpb=None, omp_num_threads=None):
+    if gpu:
+        acc_lib_cuda.threads_per_block(tpb)
+    else:
+        acc_lib_cpu.threads(omp_num_threads)
+
     str_headers = "\t".join(["size", "nsamples", "real", "OMP", "tpb", "Ttot", "Tavg", "Tstd", "Tmin"])
     if options.output_header:
         with open(options.output, 'w') as f:
             f.write(str_headers + "\n")
         exit(0)
 
-    size = options.size
-    nsamples = int(1e6)
+    cycles = options.cycles
+    number = 1
 
-    input_chi2 = setup_chi2(size, nsamples)
-    input_chi2Profile = setup_chi2Profile(size, nsamples)
+    acc_lib = 'acc_lib_cuda' if gpu else 'acc_lib_cpu'
 
-    if not options.TIMING:
-        input_sample = setup_sampleImage(size, nsamples)
-
-        acc_lib.sampleImage(*input_sample)
-
-        acc_lib.chi2Image(*input_chi2)
-
+    if options.image:
+        t = timeit.Timer('from __main__ import input_data, {}; {}.chi2Image(*input_data)'.format(acc_lib, acc_lib))
     else:
-        omp_num_threads = os.environ.get('OMP_NUM_THREADS', 0)
+        t = timeit.Timer('from __main__ import input_data, {}; {}.chi2Profile(*input_data)'.format(acc_lib, acc_lib))
 
-        cycles = options.cycles
-        number = 1
-
-        if options.image:
-            t = timeit.Timer('from __main__ import input_chi2, acc_lib; acc_lib.chi2Image(*input_chi2)')
+    if options.output:
+        filename = options.output
+    else:
+        filename = "timings_"
+        if gpu:
+            filename += "GPU_{}".format(tpb)
         else:
-            t = timeit.Timer('from __main__ import input_chi2Profile, acc_lib; acc_lib.chi2Profile(*input_chi2Profile)')
-
-        if options.output:
-            filename = options.output
-        else:
-            filename = "timings_"
-            if options.USE_GPU:
-                filename += "GPU_{}".format(options.tpb)
-            else:
-                filename += "CPU_OMP_NUM_THREADS_{}".format(omp_num_threads)
+            filename += "CPU_OMP_NUM_THREADS_{}".format(omp_num_threads)
             filename += "_{}.txt".format(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
 
-        t_results = t.repeat(cycles, number)
-        # drop 1st call: invovles lots of overhead
-        str_results = "{}\t{:e}\t{}\t{}\t{}\t{:e}\t{:e}\t{:e}\t{:e}".format(size, nsamples, options.dtype, omp_num_threads, options.tpb, np.sum(t_results[1:]),
-                                  np.average(t_results[1:]), np.std(t_results[1:]), np.min(t_results))
+    t_results = t.repeat(cycles, number)
+    # drop 1st call: invovles lots of overhead
+    str_results = "{}\t{:e}\t{}\t{}\t{}\t{:e}\t{:e}\t{:e}\t{:e}".format(size, nsamples, options.dtype, omp_num_threads, tpb, np.sum(t_results[1:]),
+                                                                            np.average(t_results[1:]), np.std(t_results[1:]), np.min(t_results))
 
-        with open(filename, 'a') as f:
-            # f.write(str_headers + "\n")
-            f.write(str_results + "\n")
-            f.write("# |--> timings: {}".format(t_results) + "\n")
+    with open(filename, 'a') as f:
+        # f.write(str_headers + "\n")
+        f.write(str_results + "\n")
+        f.write("# |--> timings: {}".format(t_results) + "\n")
 
+    if not options.no_verbose:
         print(str_headers)
         print(str_results)
         print(t_results)
         print("Log saved in {}".format(filename))
+
+if __name__ == '__main__':
+
+    size = options.size
+    nsamples = int(1e6)
+
+    if options.image:
+        input_data = setup_chi2(size, nsamples)
+    else:
+        input_data = setup_chi2Profile(size, nsamples)
+
+    if options.USE_GPU:
+        for t in options.tpb:
+            do_timing(options, input_data, tpb=t, gpu=True)
+    if options.USE_CPU:
+        for nthreads in options.ompnthreads:
+            do_timing(options, input_data, omp_num_threads=nthreads)
