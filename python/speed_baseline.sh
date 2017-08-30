11a20298e655eb964210ec7bcdf4ad4b0869f4eb
Marco Tazzari <mtazzari@ast.cam.ac.uk>
2017-08-30 18:22:56 +0100

    [speed] Differentiate benchmark for profile and image

--- python/speed_baseline.sh
+++ python/speed_baseline.sh
@@ -5,28 +5,42 @@ set -e
 
 # output file name is first arg
 if [ -z "$1" ]; then
-    output="baseline_$(git rev-parse --short HEAD)_$HOSTNAME.log"
+    output="baseline_$(git rev-parse --short HEAD)_$HOSTNAME.txt"
 else
     output=$1
 fi
 
-# recreate empty file
-:> $output
-cycles=10
-
-openmp_threads="1 2 4 8 16 32 48 64"
-threads_per_block="32 64 128 256 512 1024"
+openmp_threads="1 2 4 6 8 10 12"
+threads_per_block="8 16 32"
 sizes="512 1024 2048 4096 8192 16384"
+cycles=20
 
 # quick testing
-# openmp_threads="1 16"
-# threads_per_block="128 256"
-# sizes="512 1024"
+#openmp_threads="6"
+#threads_per_block="32"
+#sizes="4096"
+#cycles=10
 
-python speed_benchmark.py --output_header --output=$output;
+# profile
+output1=profile_${output}
+:> $output1 # recreate empty file
 
-cmd="python speed_benchmark.py --timing --cycles=$cycles --output=${output} --no-verbose"
+python speed_benchmark.py --output_header --output=$output1;
+
+cmd="python speed_benchmark.py --timing --cycles=$cycles --output=${output1} --no-verbose"
 
 for s in $sizes; do
-    $cmd --size=$s --gpu --tpb ${threads_per_block} --ompnthreads ${openmp_threads} >> $output 2>&1;
+    $cmd --size=$s --gpu --tpb ${threads_per_block} --ompnthreads ${openmp_threads} >> $output1 2>&1;
+done
+
+# image
+output2="image_${output}"
+:> $output2 # recreate empty file
+
+python speed_benchmark.py --output_header --output=${output2} --image;
+
+cmd="python speed_benchmark.py --timing --cycles=$cycles --output=${output2} --no-verbose --image"
+
+for s in $sizes; do
+    $cmd --size=$s --gpu --tpb ${threads_per_block} --ompnthreads ${openmp_threads} >> $output2 2>&1;
 done
